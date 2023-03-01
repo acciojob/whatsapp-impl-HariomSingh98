1ddc4946d178e02a3ee872a38df9ee254c1ebd83
Hariomsingh98 <hariomjawadi100@gmail.com>
2023-03-01 21:10:07 +0530

    submit2

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,6 +1,7 @@
 package com.driver;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -9,38 +10,37 @@ import java.util.List;
 @Service
 
 public class WhatsappService {
-
-   WhatsappRepository repo = new WhatsappRepository();
+    WhatsappRepository repo = new WhatsappRepository();
 
    public String createUser(String name,String mobile) throws Exception
    {
-       return createUser(name,mobile);
+       return repo.createUser(name,mobile);
    }
 
    public Group createGroup(List<User> members)
    {
-       return createGroup(members);
+       return repo.createGroup(members);
    }
 
    public int createMessage(String content){
-       return createMessage(content);
+       return repo.createMessage(content);
    }
     public int sendMessage(Message message, User sender, Group group) throws Exception
     {
-        return sendMessage(message,sender,group);
+        return repo.sendMessage(message,sender,group);
     }
 
     public String changeAdmin(User approver, User user, Group group) throws Exception{
 
-       return changeAdmin(approver, user, group);
+       return repo.changeAdmin(approver, user, group);
     }
 
     public int removeUser(User user) throws Exception{
-       return removeUser(user);
+       return repo.removeUser(user);
     }
 
     public String findMessage(Date start, Date end, int K) throws Exception{
-       return findMessage(start, end, K);
+       return repo.findMessage(start, end, K);
     }
 
 
